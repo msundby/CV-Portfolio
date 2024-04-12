496c18953246ecb26e2e8970e77e809dfa8da015
msundby <114078619+msundby@users.noreply.github.com>
2024-04-13 00:36:03 +0200

    Projects component finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FrontpageComponent],
+  imports: [RouterOutlet, NavbarComponent, FrontpageComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
