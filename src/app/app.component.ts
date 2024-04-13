6f3776f97833235b20af006368c77aee0445f10a
msundby <114078619+msundby@users.noreply.github.com>
2024-04-13 17:13:12 +0200

    Timeline updated with a few highlights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FrontpageComponent, ProjectsComponent],
+  imports: [RouterOutlet, NavbarComponent, FrontpageComponent, ProjectsComponent, TimelineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   daisyui: {
     theme: {
+      extend: {
+        fontFamily: {
+          roboto: ["Roboto","serif"]
+        },
+      }
     },
   },
   plugins: [require("daisyui")],
