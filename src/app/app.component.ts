748d6946338a9189ac5a7cbb55b12d13ac7be3e5
msundby <114078619+msundby@users.noreply.github.com>
2024-04-19 02:38:17 +0200

    New about me page up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FrontpageComponent, ProjectsComponent, TimelineComponent],
+  imports: [RouterOutlet, NavbarComponent, FrontpageComponent, ProjectsComponent, TimelineComponent, AboutmeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
